a3e67bd20d616bb9084b0ba26ff0ca664cf78398
EDBON <edfaborivera@gmail.com>
2025-04-23 15:51:57 -0500

    settings.py

--- Novasen/Novasen/settings.py
+++ Novasen/Novasen/settings.py
@@ -25,7 +25,7 @@ AUTH_USER_MODEL = 'Novasen_app.Usuario'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='hsjhgasdghadjhsjh')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='hsjhgasdghadjhsjh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
