b47faecfb64da9f15198ceecf10fa34a90d5b789
EDBON <edfaborivera@gmail.com>
2025-04-23 15:10:00 -0500

    Procfile, render.yaml, requirements.txt

--- Novasen/Novasen/settings.py
+++ Novasen/Novasen/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,17 @@ AUTH_USER_MODEL = 'Novasen_app.Usuario'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#unhl=^b@ikyp#+*4z)p04(^p*-fhtnkbpmp-grsq%)xpz!hzq'
+
+SECRET_KEY = os.enviroment.get('SECRET_KEY', default='hsjhgasdghadjhsjh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get ('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Novasen.wsgi --log-file -
